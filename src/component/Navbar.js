3f9e5c2e7911bdcc29c0535e17f3defe7743c651
NutArissara <6310110564@psu.ac.th>
2023-06-06 19:48:21 +0700

    navbar edit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,15 @@
-import React, { Component} from "react";
+import React, { Component, useRef} from "react";
 import Navitem from './Navitems'
 
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-nav-toggle');
+
+navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle('active');
+})
+
 class Navbar extends Component {
+
     constructor(props) {
         super(props);
         
@@ -23,7 +31,15 @@ class Navbar extends Component {
     render() {
         return(
         <nav>
-            <ul>
+            <span className="nav-toggle" id="js-nav-toggle">
+                <i class="fas fa-bars"></i>
+            </span>
+
+            <div className="nav-r">
+                <h3>PORTFOLIO</h3>
+            </div>
+
+            <ul className="nav-l" id="js-menu">
                 <Navitem item='Home' tolink="/" activenav={this.activeitem}></Navitem>
                 <Navitem item='About' tolink="/about" activenav={this.activeitem}></Navitem>
                 <Navitem item='Education' tolink="/education" activenav={this.activeitem}></Navitem>
